3b1e034cd29880fbc91e9179653743e19ebb1070
3KR4 <3KR4@GMAIL.COM>
2024-11-21 18:11:33 +0200

    a

--- app/api/send-email-custom/route.js
+++ app/api/send-email-custom/route.js
@@ -1,7 +1,7 @@
 import { Resend } from "resend";
 import CustomPlanTemplate from '../../_components/CustomPlanTemplate';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend('re_GwvpQ5dE_CjFsCByDxcXFQcjeGtvWownC');
 
 export async function POST(request) {
   try {
